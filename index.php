d807e9d7d848d4f8fe4fb27a82ccd3ef59db5a89
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-03 15:57:35 +0200

    feat: exercise base

--- index.php
+++ index.php
@@ -1,5 +1,42 @@
 <?php
 
+class Movie 
+{
+    public string $title = '';
+    public string $actors = '';
+    public string $genres = '';
+    public int $rating;
+    public string $lenguage;
+
+    function __construct($_title, $_actors, $_genres, $_rating, $_lenguage){
+        $this->title = $_title;
+        $this->actors = $_actors;
+        $this->genres = $_genres;
+        $this->rating = $_rating;
+        $this->lenguage = $_lenguage;
+    }
+    public function printElements() {
+        // Usa get_object_vars per ottenere un array associativo di tutte le proprietà
+        $properties = get_object_vars($this);
+
+        // Cicla attraverso le proprietà e stampa chiave e valore
+        foreach ($properties as $property => $value) {
+            echo "<p>" . ucfirst($property) . ": " . $value . "</p>";
+        }
+        echo "<br>";
+    }
+
+
+}
+
+$film1 = new Movie('Pulp Fiction','John Travolta', 'drammatico', 10, 'English');
+$film2 = new Movie(
+    'Il Signore degli Anelli', 
+    'Elijah Wood', 
+    'fantasy', 
+    10, 
+    'English'
+);
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +47,11 @@
     <title>Document</title>
 </head>
 <body>
+    
+        
+            <?php $film1->printElements() ?>
+            <?php $film2->printElements() ?>
+        
     
 </body>
 </html>
